15b79a34e6fa261bf26975559660c1ab90ac8a1e
Cristian Ledezma <crisledezma@gmail.com>
2022-07-29 20:14:40 -0600

    update

--- js/appManager.js
+++ js/appManager.js
@@ -29,9 +29,9 @@ export class AppManager{
     this.cardView1 = null;
     this.cardView2 = null;
 
-    this.tadaSound = new Audio('../assets/sound/tada.mp3');
-    this.loseSound = new Audio('../assets/sound/lose.mp3');
-    this.winSound = new Audio('../assets/sound/win.mp3');
+    this.tadaSound = new Audio('https://github.com/Crisledezma/Proyecto-Final-JavaScript-Cenfotec/blob/main/assets/sound/tada.mp3');
+    this.loseSound = new Audio('https://github.com/Crisledezma/Proyecto-Final-JavaScript-Cenfotec/blob/main/assets/sound/lose.mp3');
+    this.winSound = new Audio('https://github.com/Crisledezma/Proyecto-Final-JavaScript-Cenfotec/blob/main/assets/sound/win.mp3');
   }
   
   showScores(){
